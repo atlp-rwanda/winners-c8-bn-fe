ee91e98916b749f8c451db53aaed3572ce8e8e26
Pretty <alicemuhorakeye15@gmail.com>
2022-08-10 09:32:39 +0200

    solving conflict

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,8 +6,8 @@ const Dashboard = (props) => {
     <div>
       <h2 style={{color:"brown"}}><strong>This Is The DASHBOARD!</strong></h2>
       <br></br>
-      <h3>{(props.token=="")? "--- Not logged in ---" : "You are logged in with this token:"}</h3>
-      <pre style={{width:"70%"}}>{props.token}</pre>
+      <h3>{!localStorage.getItem('auth-token') ? "--- Not logged in ---" : "You are logged in with this token:"}</h3>
+      <pre style={{width:"70%"}}>{localStorage.getItem('auth-token')}</pre>
     </div>
   );
 }
