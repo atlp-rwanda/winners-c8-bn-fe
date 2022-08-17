d6bd2a69fddec3f6bad610a2b49e8512a833fe5c
alain-honore <himbazaalain022@gmail.com>
2022-08-17 13:26:43 +0200

    feat(User roles settings): writting tests

--- src/tests/UserRoleDash.test.js
+++ src/tests/UserRoleDash.test.js
@@ -1,21 +1,41 @@
 import React from "react";
 import UserRoleDash from "../components/AssignRole/UserRoleDash";
 import renderer from 'react-test-renderer'
-import { cleanup,render, act } from "@testing-library/react";
+import { cleanup,render,screen, act } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import store from '../redux/store';
 import App from '../App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { userResponse } from "./mocks/data";
 import { MemoryRouter} from 'react-router-dom';
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import configureMockStore from "redux-mock-store";
+import MockAxiosAdapter from 'axios-mock-adapter';
+import axios from "axios";
 const middleware = [thunk];
-
-
+ const mock = new MockAxiosAdapter(axios)
+ describe('User role Dashboard ', () => {
+beforeEach(() =>{
+  mock.onGet(/user/gi).reply(200, userResponse);
+})
 
   afterEach(() => {
-    cleanup;
+    cleanup();
+    window.localStorage.clear();
+    mock.reset();
   });
+  test('should render assign role component', () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <UserRoleDash />
+        </Router>
+      </Provider>
+    );
+  const userRoleElement = screen.queryByTestId('assign-roles');
+  expect(userRoleElement).not.toBeInTheDocument()
+  expect(userRoleElement).not.toEqual('Assigning Role');
+    })
 
   it('The page should render  the page and show all users', async () => {
     window.localStorage.setItem('auth-token', 'testing');
@@ -55,3 +75,4 @@ test("<UserRoleDash /> matches snapshot", ()=>{
 		.toJSON();
     expect(component).toMatchSnapshot();
 })
+ })
